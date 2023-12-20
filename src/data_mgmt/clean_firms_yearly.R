c7231271b3c172ec7b99888395629e15e0da76a3
gsl <guillermo.sanchez08@gmail.com>
2023-12-20 12:05:25 -0300

    Rename firms yearly

--- src/data_mgmt/clean_firms_yearly.R
+++ src/data_mgmt/clean_firms_yearly.R
@@ -1,11 +1,14 @@
 library(fastverse)
 library(fst)
 library(lubridate)
+library(purrr)
 
 source("src/lib/cli_parsing_o.R")
 source("src/lib/stata_helpers.R")
 
 # Balances --------------------------------------------------------------------
+
+message("Processing corporate income tax affidavits and balance sheets.")
 
 bal <- read_fst("src/data/dgi_firmas/out/data/balances_allF_allY.fst", as.data.table = TRUE)
 
@@ -29,14 +32,18 @@ bal[irae > CorpTaxDue, CorpTaxDue := CorpTaxDue + 1]
 
 cbal <- bal |>
     collap(turnover + Revenue + Cost + Profit + CorpTaxDue ~ fid + year, fsum)
+rm(bal)
 
 # DJ de ventas por IVA ---------------------------------------------------------
 
-sls <- read_fst("src/data/dgi_firmas/out/data/sales_allF_allY.fst", as.data.table = TRUE)
-csls <- sls |>
+message("Processing VAT affidavits and taxable sales/purchases.")
+
+csls <- read_fst("src/data/dgi_firmas/out/data/sales_allF_allY.fst", as.data.table = TRUE) |>
     collap(vatSales + vatPurchases + vatDue + vatLiability + turnoverNetOfTax + taxableTurnover ~ fid + year, fsum)
 
-# Pagos de IVA -----------------------------------------------------------------
+# Pagos y retenciones de impuestos ---------------------------------------------
+
+message("Processing tax paid and tax retained by third parties.")
 
 tax <- read_fst("src/data/dgi_firmas/out/data/tax_paid_retained.fst", as.data.table = TRUE)
 tax[, year := lubridate::year(date)]
@@ -51,38 +58,27 @@ ctax <- tax |>
         fsum
 )
 for (v in taxvarlist) ctax[, (v) := get(v) * 1e03]
+rm(tax)
 
 # e-ticket ---------------------------------------------------------------------
 
-cfe <- read_fst("src/data/dgi_firmas/out/data/eticket_transactions.fst", as.data.table = TRUE)
+message("Processing e-ticket transaction data.")
 
-ntickets <- merge(
-    collap(cfe, nTickets ~ id_receptor + year, fsum) |>
-        setnames(c("id_receptor", "nTickets"), c("fid", "nTicketsReceived")),
-    collap(cfe, nTickets ~ id_emisor + year, fsum) |>
-        setnames(c("id_emisor", "nTickets"), c("fid", "nTicketsEmitted")),
-    by = c("fid", "year"), all = TRUE
-)
-
-amount <- merge(
-    collap(cfe[(positiveAmount)], grossAmount + netAmount ~ id_receptor + year, fsum) |>
-        setnames(c("id_receptor", "grossAmount", "netAmount"), c("fid", "grossAmountReceived", "netAmountReceived")),
-    collap(cfe[(positiveAmount)], grossAmount + netAmount ~ id_emisor + year, fsum) |>
-        setnames(c("id_emisor", "grossAmount", "netAmount"), c("fid", "grossAmountEmitted", "netAmountEmitted")),
-    by = c("fid", "year"), all = TRUE
-)
+cfetab <- read_fst("out/data/eticket_yearly.fst", as.data.table = TRUE)
 
 # Merge ------------------------------------------------------------------------
+
+message("Merging all data sources.")
 
 ipcy <- fread("src/data/ipc_deflactor_2016m12.csv") %>% 
     .[lubridate::month(date) == 12] %>%
     .[, year := lubridate::year(date)]
 
-lapply(list(cbal, csls, ctax), setkeyv, c("fid", "year")) |> invisible()
-dt <- merge(cbal, csls, all = TRUE) %>%
-    merge(ctax, all = TRUE) %>%
-    merge(ntickets, all.x = TRUE) %>%
-    merge(amount, all.x = TRUE) %>%
+dtlist <- list(cbal, csls, ctax)
+lapply(dtlist, setkeyv, c("fid", "year")) |> invisible()
+dt <- dtlist |>
+    purrr::reduce(merge, all = TRUE) %>%
+    merge(cfetab, all.x = TRUE) %>%
     .[inrange(year, 2010, 2016)] %>%
     merge(fread("src/data/ui.csv"), all.x = TRUE, by = "year") |>
     merge(ipcy[, .(year, defl)], by = "year") 
@@ -98,5 +94,7 @@ for (v in varlist) dt[, (paste0(v, "M")) := get(v) / 1e06]
 for (v in varlist) dt[, (paste0(v, "MUI")) := get(v) / ui]
 
 # Export ----------------------------------------------------------------------
+
+message("Writing output: ", opt$output)
 
 write_fst(dt, opt$output)
