fd992c85eaafd23b2c9280f8fff688fdc8de8aff
chezlag <guillermo.sanchez08@gmail.com>
2024-01-03 14:50:14 -0300

    Fix static firm data merge

--- src/data_mgmt/clean_firms_static.R
+++ src/data_mgmt/clean_firms_static.R
@@ -6,6 +6,6 @@ source("src/lib/cli_parsing_o.R")
 bcs <- fread("src/data/bcs_covariates.csv")
 cfe <- read_fst("out/data/eticket_static.fst", as.data.table = TRUE)
 
-dt <- merge(bcs, cfe, by = "fid")
+dt <- merge(bcs, cfe, by = "fid", all = TRUE)
 
 write_fst(dt, opt$output)
