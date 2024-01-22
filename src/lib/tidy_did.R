8d9ec598027e44b06149933306a74028864a92ea
chezlag <guillermo.sanchez08@gmail.com>
2024-01-22 19:46:56 -0300

    Add modelsummary output to tidy_did

--- src/lib/tidy_did.R
+++ src/lib/tidy_did.R
@@ -1,11 +1,12 @@
-library(modelsummary)
-library(broom)
+pkgs <- c("modelsummary", "broom")
+groundhog.library(pkgs, "2024-01-15")
 
 tidy.AGGTEobj <- function(x, ...) {
   if (x$type == "simple") {
     s <- x$DIDparams
     ret <- data.frame(
-      term = s$yname,
+      y.name = s$yname,
+      term = "Overall ATT",
       estimate = x$overall.att,
       std.error = x$overall.se,
       conf.low = x$overall.att - x$overall.se * qnorm(1 - s$alp / 2),
@@ -14,6 +15,7 @@ tidy.AGGTEobj <- function(x, ...) {
   }
   if (x$type == "dynamic") {
     ret <- data.frame(
+      y.name = s$yname, 
       term = paste0(s$yname, ", l = ", x$egt),
       estimate = x$att.egt,
       std.error = x$se.egt,
@@ -34,10 +36,19 @@ glance.AGGTEobj <- function(x, ...) {
     bstrap = s$bstrap,
     clustervars = s$clustervars,
     est.method = s$est_method,
-    panel = s$panel,
+    panel.balanced = s$panel,
     control.group = s$control_group,
     anticipation = s$anticipation
   )
+  ret
+}
+
+tidy_did_list <- function(x) {
+  ret <- list(
+    tidy = tidy.AGGTEobj(x),
+    glance = glance.AGGTEobj(x)
+  )
+  class(ret) <- "modelsummary_list"
   ret
 }
 
