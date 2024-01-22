9e4a2716b61ca82afd0e6b5843a8e86d7af29f9f
chezlag <guillermo.sanchez08@gmail.com>
2024-01-22 19:42:32 -0300

    Add failsafe for did_yearly

--- src/analysis/estimate_did_yearly.R
+++ src/analysis/estimate_did_yearly.R
@@ -76,41 +76,61 @@ map(patterns, ~ grep(.x, varlist, value = TRUE)) %>%
 message("Estimating group-time ATT.")
 ddlist <- varlist %>%
   map(
-    ~ did::att_gt(
-      yname = .x,
-      gname = "yearFirstReception",
-      idname = "fid",
-      tname = "year",
-      xformla = as.formula(params$formula) ,
-      data = dty,
-      control_group = "notyettreated",
-      weightsname = params$wt,
-      allow_unbalanced_panel = params$unbalanced,
-      clustervars = "fid",
-      est_method = "dr",
-      cores = 8
+    possibly(
+      \(x) {
+        did::att_gt(
+          yname = x,
+          gname = "yearFirstReception",
+          idname = "fid",
+          tname = "year",
+          xformla = as.formula(params$formula),
+          data = dty,
+          control_group = "notyettreated",
+          weightsname = params$wt,
+          allow_unbalanced_panel = params$unbalanced,
+          clustervars = "fid",
+          est_method = "dr",
+          cores = 8
+        )
+      }, NULL
     )
   )
 
 message("Estimating overall ATT.")
 simple <- ddlist %>%
-  map(aggte,
-    type = "simple",
-    clustervars = "fid",
-    bstrap = TRUE
+  map(
+    possibly(
+      \(x) {
+        aggte(
+          x,
+          type = "simple",
+          clustervars = "fid",
+          bstrap = TRUE
+        )
+      },
+      NULL
+    )
   )
 
 message("Estimating dynamic ATT.")
 dynamic <- ddlist %>%
-  map(aggte,
-    type = "dynamic",
-    clustervars = "fid",
-    bstrap = TRUE
+  map(
+    possibly(
+      \(x) {
+        aggte(
+          x,
+          type = "dynamic",
+          clustervars = "fid",
+          bstrap = TRUE
+        )
+      },
+      NULL
+    )
   )
 
 # Output ----------------------------------------------------------------------
 
-# save results
+message("Saving results: ", opt$output)
 saveRDS(ddlist, opt$output)
 saveRDS(simple, str_replace(opt$output, ".RDS", "_aggte.simple.RDS"))
 saveRDS(dynamic, str_replace(opt$output, ".RDS", "_aggte.dynamic.RDS"))
