2722fd74346b255aeb1c5975592b643fbb765073
chezlag <guillermo.sanchez08@gmail.com>
2024-01-25 17:17:21 -0300

    Estimate firm survival ATT
    Write up results

--- rules/analysis.smk
+++ rules/analysis.smk
@@ -2,7 +2,7 @@
 
 # --- Dictionaries --- #
 
-EST_LIST = ["S1.bal.base", "S1.bal.ctrl", "S1.bal.wt", "S2.bal.base", "S3.unbal.base", "S3.unbal.wt"] 
+EST_LIST = ["S1.bal.base", "S1.bal.ctrl", "S1.bal.wt", "S2.bal.base", "S3.unbal.base", "S3.unbal.ctrl"]
 SAMPLE_LIST = ["0", "1", "2", "3"]
 SPEC_LIST = ["base", "ctrl", "wt"]
 PANEL_LIST = ["bal", "unbal"]
@@ -10,13 +10,15 @@ PANEL_LIST = ["bal", "unbal"]
 DID_YEARLY = expand("out/analysis/did_yearly_{estimates}.RDS", estimates = EST_LIST)
 DID_YEARLY_BYV = expand("out/analysis/did_yearly_by.{byvar}_{estimates}.RDS", 
     byvar = ["size", "industry"], estimates = ["S1.bal.base", "S3.unbal.base"])
+DID_YEARLY_SURV = expand("out/analysis/did_yearly_survival_{estimates}.RDS", estimates = ["S1.bal.base", "S3.bal.base"])
 
 # --- Target rules --- #
 
 rule did:
     input:
         DID_YEARLY,
-        DID_YEARLY_BYV
+        DID_YEARLY_BYV,
+        DID_YEARLY_SURV
 
 # --- Build rules --- #
 
@@ -35,7 +37,7 @@ rule estimate_did_yearly:
     log:
         "logs/analysis/" + "estimate_did_yearly_S{sample}.{panel}.{spec}.Rout"
     threads: 8
-    wildcard_constraints: 
+    wildcard_constraints:
         sample = "|".join(SAMPLE_LIST),
         panel = "|".join(PANEL_LIST),
         spec = "|".join(SPEC_LIST)
@@ -100,3 +102,30 @@ rule estimate_did_yearly_by_industry:
         --spec {input.params_spec} \
         --output {output.est1} \
          > {log} {logAll}"
+
+rule estimate_did_yearly_survival:
+    input:
+        script = "src/analysis/" + "estimate_did_yearly_survival.R",
+        data = "out/data/" + "firms_yearly_filled.fst",
+        samples = "out/data/" + "samples.fst",
+        params_sample = "src/model_specs/" + "sample_{sample}.json",
+        params_panel = "src/model_specs/" + "panel_{panel}.json",
+        params_spec = "src/model_specs/" + "spec_{spec}.json"
+    output:
+        est1 = "out/analysis/" + "did_yearly_survival_S{sample}.{panel}.{spec}.RDS",
+        est2 = "out/analysis/" + "did_yearly_survival_S{sample}.{panel}.{spec}_aggte.simple.RDS",
+        est3 = "out/analysis/" + "did_yearly_survival_S{sample}.{panel}.{spec}_aggte.dynamic.RDS"
+    log:
+        "logs/analysis/" + "estimate_did_yearly_survival_S{sample}.{panel}.{spec}.Rout"
+    threads: 8
+    wildcard_constraints: 
+        sample = "|".join(SAMPLE_LIST),
+        panel = "|".join(PANEL_LIST),
+        spec = "|".join(SPEC_LIST)
+    shell:
+        "{runR} {input.script} \
+            --sample {input.params_sample} \
+            --panel {input.params_panel} \
+            --spec {input.params_spec} \
+            --output {output.est1} \
+            > {log} {logAll}"
---
+++ src/analysis/estimate_did_yearly_survival.R
@@ -0,0 +1,119 @@
+library(groundhog)
+pkgs <- c(
+  "data.table",
+  "magrittr",
+  "fst",
+  "purrr",
+  "stringr",
+  "did",
+  "jsonlite",
+  "arsenal"
+)
+date <- "2024-01-15"
+groundhog.library(pkgs, date)
+source("src/lib/cli_parsing_om.R")
+
+message("Parsing model parameters.")
+message("Sample: ", opt$sample)
+message("Panel: ", opt$panel)
+message("Spec: ", opt$spec)
+params <- list(
+  opt$sample,
+  opt$panel,
+  opt$spec
+) %>%
+  map(fromJSON) %>%
+  unlist(recursive = FALSE)
+
+# Input -----------------------------------------------------------------------
+
+message("Reading data and setting up variables.")
+
+sample <-
+  read_fst("out/data/samples.fst", as.data.table = TRUE) %>%
+  .[eval(parse(text = params$sample_fid)), .(fid)]
+dtycj <-
+  read_fst("out/data/firms_yearly_filled.fst", as.data.table = TRUE) %>%
+  .[sample, on = "fid"] %>%
+  .[eval(parse(text = params$sample_yearly))] %>% 
+  .[year %in% 2010:2016]
+
+# size and age quartiles – sample specific
+quartiles <- dtycj[, quantile(Scaler1, probs = seq(0, 1, 0.25), na.rm = TRUE)]
+dtycj[, sizeQuartile := cut(Scaler1, breaks = quartiles, labels = 1:4)]
+quartiles <- dtycj[, quantile(firm_age, probs = seq(0, 1, 0.25), na.rm = TRUE)]
+dtycj[, ageQuartile := cut(firm_age, breaks = quartiles, labels = 1:4)]
+
+# outcome variable list
+varlist <- c(
+  "anyVatPaid", 
+  "anyCorpTaxPaid",
+  "anyOtherTaxPaid",
+  "anyTaxPaid",
+  "anyRecordedActivity"
+)
+
+# Estimate --------------------------------------------------------------------
+
+message("Estimating group-time ATT.")
+ddlist <- varlist %>%
+  map(
+    possibly(
+      \(x) {
+        did::att_gt(
+          yname = x,
+          gname = "yearFirstReception",
+          idname = "fid",
+          tname = "year",
+          xformla = as.formula(params$formula),
+          data = dtycj,
+          control_group = "notyettreated",
+          weightsname = params$wt,
+          allow_unbalanced_panel = params$unbalanced,
+          clustervars = "fid",
+          est_method = "dr",
+          cores = 8,
+          anticipation = 1
+        )
+      }, NULL
+    )
+  )
+
+message("Estimating overall ATT.")
+simple <- ddlist %>%
+  map(
+    possibly(
+      \(x) {
+        aggte(
+          x,
+          type = "simple",
+          clustervars = "fid",
+          bstrap = TRUE
+        )
+      },
+      NULL
+    )
+  )
+
+message("Estimating dynamic ATT.")
+dynamic <- ddlist %>%
+  map(
+    possibly(
+      \(x) {
+        aggte(
+          x,
+          type = "dynamic",
+          clustervars = "fid",
+          bstrap = TRUE
+        )
+      },
+      NULL
+    )
+  )
+
+# Output ----------------------------------------------------------------------
+
+message("Saving results: ", opt$output)
+saveRDS(ddlist, opt$output)
+saveRDS(simple, str_replace(opt$output, ".RDS", "_aggte.simple.RDS"))
+saveRDS(dynamic, str_replace(opt$output, ".RDS", "_aggte.dynamic.RDS"))
