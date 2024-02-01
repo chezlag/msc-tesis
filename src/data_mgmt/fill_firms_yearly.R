b63d57e9bcc1760ed46761f24c2ddfab51bd9abe
chezlag <guillermo.sanchez08@gmail.com>
2024-02-01 20:56:21 -0300

    New extensive margin depvars

--- src/data_mgmt/fill_firms_yearly.R
+++ src/data_mgmt/fill_firms_yearly.R
@@ -15,6 +15,7 @@ message("Reading data and cross-joining.") # ----------------------------------
 
 dts <- read_fst("out/data/firms_static.fst", as.data.table = TRUE)
 dty <- read_fst("out/data/firms_yearly.fst", as.data.table = TRUE)
+dty[, exit := FALSE]
 
 fillCJ <- CJ(fid = unique(dty$fid), year = 2009:2016)
 dtyfill <- dty[fillCJ, on = c("fid", "year")]
@@ -31,11 +32,19 @@ dt[, firm_age := year - birth_year]
 
 message("Creating extensive margin variables.") # -----------------------------
 
+dt[is.na(exit), exit := TRUE]
+dt[is.na(in214), in214 := FALSE]
+dt[is.na(in217), in217 := FALSE]
+
 dt[, anyVatPaid := fifelse(!is.na(vatPaid), vatPaid > 0, FALSE)]
 dt[, anyCorpTaxPaid := fifelse(!is.na(corpTaxPaid), corpTaxPaid > 0, FALSE)]
 dt[, anyOtherTaxPaid := fifelse(!is.na(otherTaxPaid), otherTaxPaid > 0, FALSE)]
 dt[, anyTaxPaid := fifelse(!is.na(totalTaxPaid), totalTaxPaid > 0, FALSE)]
 dt[, anyRecordedActivity := !is.na(in214) | !is.na(in217) | !is.na(inPay)]
+
+dt[, anyRecordedPurchases := fifelse(!is.na(deductPurchases), deductPurchases > 0, FALSE)]
+dt[, anyRecordedSales := fifelse(!is.na(taxableTurnover), taxableTurnover > 0, FALSE)]
+dt[, anyRecordedRevenue := fifelse(!is.na(Revenue), Revenue > 0, FALSE)]
 
 message("Exporting cross-joined dataset: ", opt$output) # ---------------------
 
