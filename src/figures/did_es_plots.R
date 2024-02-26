5e16931bc1d78d4e3efc4924cf0e95ed494f4892
chezlag <guillermo.sanchez08@gmail.com>
2024-02-26 18:18:31 -0300

    Add new y vars and quarterly es plot

--- src/figures/did_es_plots.R
+++ src/figures/did_es_plots.R
@@ -16,8 +16,9 @@ groundhog.library(pkgs, date)
 
 source("src/lib/tidy_did.R")
 source("src/lib/theme_set.R")
+source("src/figures/gges.R")
 
-# Input  ----------------------------------------------------------------------
+# Yearly plots  ---------------------------------------------------------------
 
 speclist <- c(
   "S1_bal_ctrl_nt",
@@ -30,20 +31,38 @@ yvarlist <- c(
   "Scaled1RevenueK",
   "Scaled1vatPurchasesK",
   "Scaled1vatSalesK",
-  "Scaled1vatPaidK"
+  "Scaled1vatPaidK",
+  "Scaled1corpTaxPaidK",
+  "Scaled1totalTaxPaidK"
 )
 ylablist <- c(
   "Compras reportadas",
   "Ingreso reportado",
   "IVA Compras",
   "IVA Ventas",
-  "Pagos de IVA"
+  "Pagos de IVA",
+  "Pagos de IRAE",
+  "Pago total de impuestos"
 )
-
-# Plot ------------------------------------------------------------------------
-
-source("src/figures/gges.R")
 
 walk(speclist, \(x) walk2(yvarlist, ylablist, \(y, z) gges_all(x, y, z)))
 walk(speclist, \(x) walk2(yvarlist, ylablist, \(y, z) gges_by_industry(x, y, z)))
 walk(speclist, \(x) walk2(yvarlist, ylablist, \(y, z) gges_by_size(x, y, z)))
+
+# Quarterly plots -------------------------------------------------------------
+
+speclist <- c(
+  "S1_bal_ctrl_nyt16",
+  "S1_bal_base_nyt16"
+)
+yvarlist <- c(
+  "Scaled1vatPaidK",
+  "Scaled1corpTaxPaidK",
+  "Scaled1totalTaxPaidK"
+)
+ylablist <- c(
+  "Pagos de IVA",
+  "Pagos de IRAE",
+  "Pago total de impuestos"
+)
+walk(speclist, \(x) walk2(yvarlist, ylablist, \(y, z) gges_quarterly(x, y, z)))
--- src/figures/gges.R
+++ src/figures/gges.R
@@ -152,3 +152,49 @@ gges_by_size <- function(spec, yvar, ylab) {
     units = "mm"
   )
 }
+
+gges_quarterly <- function(spec, yvar, ylab) {
+  est <- readRDS(paste0("out/analysis/did.q.all.", spec, ".RDS"))
+  tidy <- est$dynamic[[yvar]] |> tidy_did() |> setDT()
+  attgt <- est$attgt[[yvar]]
+  simple <- est$simple[[yvar]]
+
+  sig <- ifelse(
+    inrange(0, tidy_did(simple)$conf.low, tidy_did(simple)$conf.high),
+    "",
+    "**"
+  )
+
+  tidy[y.name == yvar & inrange(event, -20, 12)] %>%
+    .[, treat := fifelse(event < 0, "Pre", "Post")] %>%
+    ggplot(aes(x = event, y = estimate, color = treat, fill = treat)) +
+    geom_point(size = 2) +
+    geom_rect(
+      aes(
+        ymin = conf.low,
+        ymax = conf.high,
+        xmin = event - 0.1,
+        xmax = event + 0.1,
+        fill = treat
+      ),
+      alpha = 0.4,
+      inherit.aes = FALSE
+    ) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    scale_x_continuous(breaks = seq(-20, 12, 4)) +
+    scale_y_continuous(labels = scales::dollar_format()) +
+    scale_color_startrek() +
+    scale_fill_startrek() +
+    labs(
+      x = "Trimestres desde el tratamiento", y = ylab,
+      caption = paste0("Overall ATT: ", round(simple$overall.att, 3), sig)
+    ) +
+    theme(legend.position = "none")
+
+  ggsave(
+    paste0("out/figures/did.q.all.", yvar, ".", spec, ".png"),
+    width = 170,
+    height = 100,
+    units = "mm"
+  )
+}
