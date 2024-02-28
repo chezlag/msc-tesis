0c92dfa8d35a60b005b4ef104ec301e93049189e
chezlag <guillermo.sanchez08@gmail.com>
2024-02-28 13:56:28 -0300

    fig: Add vline to takeup at compulsory e-invoicing

--- src/figures/takeup.R
+++ src/figures/takeup.R
@@ -13,9 +13,19 @@ dts <-
 dts[is.na(dateFirstEmission), dateFirstEmission := ymd("2020-01-01")]
 dts[is.na(dateFirstReception), dateFirstReception := ymd("2020-01-01")]
 
+textdt <- data.frame(
+  x = ymd("2015-09-01"),
+  y = 0.75,
+  label = "Emisión obligatoria\npara empresas con\ningresos > 30M UI\n––>"
+)
+
 p1 <- dts %>%
   ggplot() +
   stat_ecdf(aes(dateFirstEmission), geom = "step") +
+  geom_vline(
+    xintercept = ymd("2016-06-01"),color = "maroon", linetype = "dashed") +
+  scale_x_date(expand = c(0, 0.1)) +
+  scale_y_continuous(limits = c(0, 1), expand = c(0, 0.01)) +
   coord_cartesian(xlim = c(ymd("2012-01-01"), ymd("2016-11-30"))) +
   labs(
     x = "Fecha de primera emisión", y = "Función de distribución acumulada",
@@ -26,6 +36,11 @@ p1 <- dts %>%
 p2 <- dts %>%
   ggplot() +
   stat_ecdf(aes(dateFirstReception), geom = "step") +
+  geom_vline(
+    xintercept = ymd("2016-06-01"), color = "maroon", linetype = "dashed"
+  ) +
+  scale_x_date(expand = c(0, 0.1)) +
+  scale_y_continuous(limits = c(0, 1), expand = c(0, 0.01)) +
   coord_cartesian(xlim = c(ymd("2012-01-01"), ymd("2016-11-30"))) +
   labs(
     x = "Fecha de primera recepción", y = NULL,
