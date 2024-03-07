2e9c2d435326941aadf29c7148675715ed2f7c1d
chezlag <guillermo.sanchez08@gmail.com>
2024-03-07 17:11:10 -0300

    fig(share_zeros): Add vatPaid to plot
    And change some labels.

--- src/figures/share_zeros.R
+++ src/figures/share_zeros.R
@@ -14,8 +14,7 @@ dty <-
   read_fst("out/data/firms_yearly.fst", as.data.table = TRUE) %>%
   .[sample, on = "fid"]
 
-dty[, netVatDue := fifelse(vatDue >= 0, vatDue, 0)]
-varlist <- c("vatPurchases", "vatSales", "vatDue")
+varlist <- c("vatPurchases", "vatSales", "vatDue", "vatPaid")
 for (v in varlist) dty[, (paste0(v, "0")) := get(v) <= 0]
 
 tab <- melt(dty, id.vars = c("fid", "year"), measure.vars = paste0(varlist, "0")) %>%
@@ -23,16 +22,17 @@ tab <- melt(dty, id.vars = c("fid", "year"), measure.vars = paste0(varlist, "0")
 tab[, variable := fcase(
   variable == "vatPurchases0", "IVA Compras",
   variable == "vatSales0", "IVA Ventas",
-  variable == "vatDue0", "IVA adeudado"
+  variable == "vatDue0", "IVA adeudado",
+  variable == "vatPaid0", "Pagos de IVA"
 )]
 
 # Plot ------------------------------------------------------------------------
 
-tab %>%
+tab[year < 2016] %>%
   ggplot(aes(year, value, color = variable)) +
   geom_point() +
   geom_line() +
   ylim(0, NA) +
-  labs(x = "Años", y = "Proporción de ceros", color = NULL)
+  labs(x = "Años", y = "Proporción de empresas con Y=0", color = NULL)
 
 ggsave("out/figures/share_zeros.png", width = 170, height = 100, units = "mm")
