24430c1ec67a0ab8dabcd4826bb33fe08650d246
chezlag <guillermo.sanchez08@gmail.com>
2024-03-08 20:19:28 -0300

    fig: Event study by size

---
+++ src/figures/es.did.y.by_size.R
@@ -0,0 +1,47 @@
+library(groundhog)
+pkgs <- c(
+  "collapse",
+  "data.table",
+  "did",
+  "forcats",
+  "ggplot2",
+  "ggsci",
+  "magrittr",
+  "purrr",
+  "scales",
+  "stringr"
+)
+date <- "2024-01-15"
+groundhog.library(pkgs, date)
+
+source("src/lib/cli_parsing_s.R")
+source("src/lib/tidy_did.R")
+source("src/lib/theme_set.R")
+source("src/figures/gges.R")
+
+# Yearly plots  ---------------------------------------------------------------
+
+yvarlist <- c(
+  "Scaled1vatPurchasesK",
+  "Scaled1vatSalesK",
+  "Scaled1netVatLiabilityK",
+  "Scaled1vatPaidK",
+  "vatPurchases0",
+  "vatSales0",
+  "netVatLiability0",
+  "vatPaid0"
+)
+ylablist <- c(
+  "IVA Compras",
+  "IVA Ventas",
+  "IVA adeudado",
+  "Pago de IVA",
+  "P(IVA Compras > 0)",
+  "P(IVA Ventas > 0)",
+  "P(IVA adeudado > 0)",
+  "P(Pago de IVA > 0)"
+)
+y_dollar <- c(rep(TRUE, 4), rep(FALSE, 4))
+params <- list(yvarlist, ylablist, y_dollar)
+
+pwalk(params, \(x, y, z) gges_by_size(opt$spec, x, y, 136, 80, y_dollar = z))
