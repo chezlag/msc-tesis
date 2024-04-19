f042573b6dc4e9f3dc7d3add6e8a0234f3ebd6a8
chezlag <guillermo.sanchez08@gmail.com>
2024-04-18 23:30:11 -0300

    feat: Define new treatment cohorts

--- src/data_mgmt/define_cohorts.R
+++ src/data_mgmt/define_cohorts.R
@@ -4,7 +4,7 @@ groundhog.library(pkgs, "2024-01-15")
 source("src/lib/cli_parsing_o.R")
 
 dty <- read_fst("out/data/firms_yearly.fst", as.data.table = TRUE)
-dtq <- read_fst("out/data/firms_quarterly.fst", as.data.table = TRUE)
+mdt <- fread("src/data/dgi_firmas/out/data/mandatory_emission.csv")
 
 # Define lookup tables of fid --------------------------------------------------
 
@@ -15,20 +15,38 @@ lookup_list[[1]] <-
   dty[(received), .(G1 = fmin(year) |> as.double()), fid]
 
 # Year received e-tickets share is over median
-dty[, eticketTax := grossAmountReceived - netAmountReceived]
-dty[, eticketTaxShare := eticketTax / vatPurchases]
+dty[, eticketTax := netAmountReceived]
+dty[, eticketTaxShare := eticketTax / Scaler3]
 median <- dty[, fmedian(eticketTaxShare)]
 dty[, receivedSzOverMedian := fifelse(
   !is.na(eticketTaxShare),
   eticketTaxShare > median,
   FALSE
 )]
+dty[, receivedSzBelowMedian := fifelse(
+  !is.na(eticketTaxShare),
+  eticketTaxShare < median,
+  FALSE
+)]
 lookup_list[[2]] <-
   dty[(receivedSzOverMedian), .(G2 = fmin(year) |> as.double()), fid]
-
-# Quarter received first e-ticket reception
 lookup_list[[3]] <-
-  dtq[(received), .(G3 = fmin(quarter) |> as.double()), fid]
+  dty[(receivedSzBelowMedian), .(G3 = fmin(year) |> as.double()), fid]
+
+# Mandated e-ticket adoption
+dty[, yearMandatedTurnover := fcase(
+  year == 2015 & turnoverMUI > 15, 2016,
+  year == 2016 & turnoverMUI > 4, 2017,
+  default = Inf
+)]
+mandated <- merge(
+  dty[, fmin(yearMandatedTurnover), fid],
+  mdt,
+  by = "fid"
+)
+mandated[, G4 := fifelse(!is.na(mandatedEmissionYear), mandatedEmissionYear, V1)]
+lookup_list[[4]] <- mandated[, .(fid, G4, mandated)]
+
 
 # Define treatment groups  -----------------------------------------------------
 
@@ -40,8 +58,7 @@ lut <- list(
   reduce(merge, by = "fid", all = TRUE)
 varlist <- paste0("G", seq_along(lookup_list))
 for (v in varlist) lut[is.na(get(v)), (v) := Inf]
-lut[, G3 := lubridate::as_date(G3)]
 
 # Export  ----------------------------------------------------------------------
-
+opt$output <- "out/data/cohorts.fst"
 write_fst(lut, opt$output)
