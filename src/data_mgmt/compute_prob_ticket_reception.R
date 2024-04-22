5a764f6de94f248a1ff8de2b8dae2bc22d23f0ed
gsl <guillermo.sanchez08@gmail.com>
2024-04-22 12:28:36 -0700

    feat: Compute probability of receiving e-ticket
    Instrument for IV

--- src/data_mgmt/compute_prob_ticket_reception.R
+++ src/data_mgmt/compute_prob_ticket_reception.R
@@ -42,40 +42,58 @@ fwrite(tab[!is.na(supply)], "out/data/share_eticket_by_industry.csv")
 
 # Percent of inputs provided by each sector -----------------------------------
 
-cou <- read_excel(
-  "src/data/2016_Agregada_Utilizacion intermedia Nacional_pc_C.xlsx",
-  skip = 7
-) |>
-  clean_names()
-setDT(cou)
-sectorlist <- tab[, .N, supply][!is.na(supply), supply]
-cou <- melt(
-  cou[!is.na(as.integer(codigo))],
-  id.vars = "codigo",
-  measure.vars = sectorlist,
-  variable.name = "demand"
-)
-cou[, supply := fcase(
-  codigo %in% 1:3, "a_b",
-  codigo %in% 5:9, "c",
-  codigo == 4, "d_e",
-  codigo == 10, "f",
-  codigo == 11, "g_i",
-  codigo %in% c(12, 13, 17), "h_j",
-  codigo == 14, "k",
-  codigo == 15, "l",
-  codigo == 16, "m_n",
-  codigo == 18, "o",
-  codigo %in% 19:21, "p_q_r_s_t"
-)]
-cou <- collap(cou, value ~ supply + demand, fsum)
 
-dmd <- merge(
-  cou,
-  collap(cou, value ~ demand, fsum) |> setnames("value", "total"),
-  by = "demand"
-)
-dmd[, szDemand := value / total]
+clean_cou <- function(file, range) {
+  `:=` <- `.N` <- supply <- NULL
+  cou <- readxl::read_excel(
+    file,
+    range = range
+  ) |>
+    janitor::clean_names()
+  data.table::setDT(cou)
+  sectorlist <- tab[, .N, supply][!is.na(supply), supply]
+  cou <- data.table::melt(
+    cou[!is.na(as.integer(codigo))],
+    id.vars = "codigo",
+    measure.vars = sectorlist,
+    variable.name = "demand"
+  )
+  cou[, supply := data.table::fcase(
+    codigo %in% 1:3, "a_b",
+    codigo %in% 5:9, "c",
+    codigo == 4, "d_e",
+    codigo == 10, "f",
+    codigo == 11, "g_i",
+    codigo %in% c(12, 13, 17), "h_j",
+    codigo == 14, "k",
+    codigo == 15, "l",
+    codigo == 16, "m_n",
+    codigo == 18, "o",
+    codigo %in% 19:21, "p_q_r_s_t"
+  )]
+  ret <- collapse::collap(cou, value ~ supply + demand, fsum)
+  ret
+}
+
+cou12 <- clean_cou("src/data/2012_Agregada_COU_C.xlsx", "A36:X57")
+cou16 <- clean_cou("src/data/2016_Agregada_Utilizacion intermedia Nacional_pc_C.xlsx", "A8:R29")
+fwrite(cou12, "out/data/cou12.csv")
+fwrite(cou16, "out/data/cou16.csv")
+
+compute_demand_share <- function(dt) {
+  `:=` <- szDemand <- value <- NULL # avoid NSE warnings
+  ret <- merge(
+    dt,
+    collapse::collap(dt, value ~ demand, fsum) |> data.table::setnames("value", "total"),
+    by = "demand"
+  )
+  ret[, szDemand := value / total]
+  ret
+}
+dmd12 <- compute_demand_share(cou12)
+dmd16 <- compute_demand_share(cou16)
+
+dmd <- dmd12
 
 # Probability of receiving e-ticket by sector --------------------------------
 
