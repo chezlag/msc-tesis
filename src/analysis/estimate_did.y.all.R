75889f60ef407db6a9cebb06fbfa8b29a984afe8
gsl <guillermo.sanchez08@gmail.com>
2024-04-22 13:30:27 -0700

    refactor: did estimates - keep & order by fig & tables

--- src/analysis/estimate_did.y.all.R
+++ src/analysis/estimate_did.y.all.R
@@ -1,140 +1,112 @@
 library(groundhog)
 pkgs <- c(
-  "data.table",
-  "magrittr",
-  "fst",
-  "purrr",
-  "stringr",
+  "arsenal",
   "did",
+  "fastverse",
+  "fst",
   "jsonlite",
-  "arsenal"
+  "purrr",
+  "stringr"
 )
 groundhog.library(pkgs, "2024-01-15")
+set.seed(20240115)
 source("src/lib/cli_parsing_om.R")
 source("src/lib/winsorize.R")
-
-set.seed(20240115)
-
-message("Parsing model parameters.")
-params <- list(
-  opt$sample,
-  opt$panel,
-  opt$spec,
-  opt$group
-) %>%
-  map(fromJSON) %>%
-  unlist(recursive = FALSE)
-opt$winsorize <- paste0("0.", opt$winsorize) |> as.numeric()
+source("src/lib/cs21.R")
+source("src/lib/create_controls.R")
 
 # Input -----------------------------------------------------------------------
 
 message("Reading data and setting up variables.")
 
 sample <-
-  read_fst("out/data/samples.fst", as.data.table = TRUE) %>%
-  .[eval(parse(text = params$sample_fid)), .(fid)]
+  read_fst("out/data/samples.fst", as.data.table = TRUE)
 cohorts <-
-  read_fst("out/data/cohorts.fst", as.data.table = TRUE) %>%
-  .[eval(parse(text = params$cohorts_yearly))]
+  read_fst("out/data/cohorts.fst", as.data.table = TRUE)
 dty <-
   read_fst("out/data/firms_yearly.fst", as.data.table = TRUE) %>%
   .[sample, on = "fid"] %>%
-  merge(cohorts, by = "fid") %>%
-  .[eval(parse(text = params$sample_yearly))] %>%
-  .[year %in% 2009:2015]
+  merge(cohorts, by = "fid")
 
-# size and age quartiles – sample specific
-quartiles <- dty[, quantile(Scaler1, probs = seq(0, 1, 0.25), na.rm = TRUE)]
-dty[, sizeQuartile := cut(Scaler1, breaks = quartiles, labels = 1:4)]
-quartiles <- dty[, quantile(as.numeric(birth_date), probs = seq(0, 1, 0.25), na.rm = TRUE)]
-dty[, ageQuartile := cut(as.numeric(birth_date), breaks = quartiles, labels = 1:4)]
-dty[is.na(ageQuartile), ageQuartile := 4] # missing as young
-quartiles <- dty[, quantile(Scaler3, probs = seq(0, 1, 0.25), na.rm = TRUE)]
-dty[, assetsQuartile := cut(Scaler3, breaks = quartiles, labels = 1:4)]
-dty[is.na(assetsQuartile), assetsQuartile := floor(runif(1, 1, 5))]
+# Create samples
+dty[, sampleA := aaa]
+dty[, sampleB := aaa]
 
-# size and assets deciles – sample specific
-deciles <- dty[, quantile(Scaler3, probs = seq(0, 1, 0.1), na.rm = TRUE)] # assets
-dty[, assetsDecile := cut(Scaler3, breaks = deciles, labels = 1:10)]
-dty[is.na(assetsDecile), assetsDecile := floor(runif(1, 1, 11))]
-deciles <- dty[, quantile(Scaler1, probs = seq(0, 1, 0.1), na.rm = TRUE)] # revenue
-dty[, sizeDecile := cut(Scaler1, breaks = deciles, labels = 1:10)]
-dty[is.na(sizeDecile), sizeDecile := floor(runif(1, 1, 11))]
-
-# outcome variable list
-stubnames <- c(
-  "vatPurchases",
-  "vatSales",
-  "netVatLiability"
+# Subset samples & create controls
+stublist <- c("vatPurchasesK", "vatSalesK", "netVatLiabilityK")
+allylist <- c(
+  paste0("CR10", stublist),
+  paste0("CR0", stublist),
+  paste0("CR20", stublist),
+  paste0("CR300", stublist)
 )
-varlist <- c(
-  paste0("CR10", stubnames, "K"),
-  paste0("CR0", stubnames, "K"),
-  paste0("CR20", stubnames, "K"),
-  paste0("CR300", stubnames, "K"),
-  paste0("CR", stubnames, "KInt")
-)
-# winsorize continuous variables
-for (v in varlist) dty[, (v) := winsorize(get(v), opt$winsorize), year]
-# add extensive margin variables
-varlist <- c(
-  varlist,
-  paste0("CR", stubnames, "KExt")
+extylist <- paste0("CR", stublist, "Ext")
+xlist <- c("fid", "year", "G1", "birth_date", "Scaler1", "Scaler3", "division")
+keeplist <- c(xlist, allylist, extylist)
+sA99 <- create_controls(dt[(sampleA), ..keeplist])
+sA95 <- create_controls(dt[(sampleB), ..keeplist])
+sB99 <- create_controls(dt[(sampleB), ..keeplist])
+walk(allylist, \(v) sA99[, (v) := winsorize(get(v), .99), year])
+walk(allylist, \(v) sA95[, (v) := winsorize(get(v), .95), year])
+walk(allylist, \(v) sB99[, (v) := winsorize(get(v), .99), year])
+
+# Main results --------------------------------------------------------------------
+
+ylist <- paste0("CR10", stublist)
+dtlist <- list(sA99, sA95, sB99)
+params <- list(
+  rep(ylist, each = 3),
+  rep(dtlist, 3)
 )
 
-# Estimate --------------------------------------------------------------------
+tab1 <- map2(
+  params,
+  \(y, dt) {
+    cs21_att_gt(y, "~ sizeDecile + assetsDecile + ageQuartile + division", dt) |>
+      cs21_simple()
+  }
+)
 
-message("Estimating group-time ATT.")
-ddlist <- varlist %>%
-  map(possibly(\(x) {
-    did::att_gt(
-      yname = x,
-      gname = "G1",
-      idname = "fid",
-      tname = "year",
-      xformla = as.formula(params$formula),
-      data = dty,
-      control_group = params$control_group,
-      weightsname = params$wt,
-      allow_unbalanced_panel = params$unbalanced,
-      clustervars = "fid",
-      est_method = "dr",
-      cores = opt$threads,
-      base_period = "universal"
-    )
-  }))
+fig1 <- map(
+  ylist,
+  \(y) {
+    cs21_att_gt(y, "~ sizeDecile + assetsDecile + ageQuartile + division", sA99) |>
+      cs21_dynamic()
+  }
+)
 
-message("Estimating overall ATT.")
-simple <- ddlist %>%
-  map(possibly(\(x) {
-    aggte(
-      x,
-      type = "simple",
-      clustervars = "fid",
-      bstrap = TRUE,
-      na.rm = TRUE
-    )
-  }))
+# Robustness: Chen & Roth (2023) ------------------------------------------------
 
-message("Estimating dynamic ATT.")
-dynamic <- ddlist %>%
-  map(possibly(\(x) {
-    aggte(
-      x,
-      type = "dynamic",
-      clustervars = "fid",
-      bstrap = TRUE,
-      na.rm = TRUE
-    )
-  }))
+ylist <- allylist[c(1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12)]
 
-# Output ----------------------------------------------------------------------
+tab2 <- map(
+  params,
+  \(y, dt) {
+    cs21_att_gt(y, "~ sizeDecile + assetsDecile + ageQuartile + division", sA99) |>
+      cs21_simple()
+  }
+)
 
-# Combine results for export
-ret <- list(ddlist, simple, dynamic)
-elnames <- c("attgt", "simple", "dynamic")
-names(ret) <- elnames
-for (el in elnames) names(ret[[el]]) <- varlist
+# Extensive margin --------------------------------------------------------------
 
-message("Saving results: ", opt$output)
-saveRDS(ret, opt$output)
+ylist <- extylist
+dtlist <- list(sA99, sA95, sB99)
+params <- list(
+  rep(ylist, each = 3),
+  rep(dtlist, 3)
+)
+
+tab3 <- map2(
+  params,
+  \(y, dt) {
+    cs21_att_gt(y, "~ sizeDecile + assetsDecile + ageQuartile + division", dt) |>
+      cs21_simple()
+  }
+)
+
+# Export results ---------------------------------------------------------------
+
+saveRDS(tab1, "out/analysis/did.y.all.tab1.RDS")
+saveRDS(fig1, "out/analysis/did.y.all.fig1.RDS")
+saveRDS(tab2, "out/analysis/did.y.all.tab2.RDS")
+saveRDS(tab3, "out/analysis/did.y.all.tab3.RDS")
